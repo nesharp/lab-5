147cca0faf33a9bf1e9b1155e7105cf8910b9d5e
Michael Nesteruk <nesterukmisha27@gmail.com>
2024-03-18 19:49:29 +0200

    -false

---
+++ dijkstra/index.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getDeikstra = void 0;
+// import { dijkstra } from "./dijkstra";
+var dijkstra_1 = require("./dijkstra");
+var utils_1 = require("./utils");
+var getDeikstra = function (incomeGraph, start) {
+    var newGraph = incomeGraph.map(function (_a) {
+        var from = _a[0], to = _a[1], val = _a[2];
+        return [
+            from,
+            to,
+            val || Math.floor(Math.random() * 9 + 1).toString(),
+        ];
+    });
+    var transformedGraph = (0, utils_1.convertToDeikstra)(newGraph);
+    var graph = new dijkstra_1.Graph(transformedGraph, start);
+    return {
+        distancies: graph.distancies(),
+        dijkstra: graph.dijkstra(),
+    };
+};
+exports.getDeikstra = getDeikstra;
