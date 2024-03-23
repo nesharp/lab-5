50561590c475ce135714c27f943921975d5cae58
Michael Nesteruk <nesterukmisha27@gmail.com>
2024-03-23 15:44:14 +0200

    hash fixed

--- belman/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getBelman = void 0;
-var belman_1 = require("./classes/belman");
-var getBelman = function (graph, start) {
-    var graphInstance = new belman_1.Graph(graph, start);
-    return graphInstance.getBelman();
-};
-exports.getBelman = getBelman;
--- belman/index.ts
+++ belman/index.ts
@@ -1,7 +1,4 @@
 import { Graph } from "./classes/belman";
-import { graph } from "./settings";
-import { convertToString, transformGraph } from "./utils";
-
 export const getBelman = (graph: string[][], start: string) => {
   const graphInstance = new Graph(graph, start);
   return graphInstance.getBelman();
